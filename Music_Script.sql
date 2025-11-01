d826c059e1f74fada11c788c4b3c3e190d007612
Neto test <kordik.a@yndex.ru>
2025-11-01 20:12:00 +0300

    Initial

---
+++ Music_Script.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS Author(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS Genres(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS GenresAuthor (
+	author_id INTEGER REFERENCES Author(id),
+	genre_id INTEGER REFERENCES Genres(id),
+	CONSTRAINT pk PRIMARY KEY (author_id, genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS Album(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	released_date date
+);
+
+
+CREATE TABLE IF NOT EXISTS AuthorAlbum(
+	author_id INTEGER REFERENCES Author(id),
+	album_id INTEGER REFERENCES Album(id),
+	CONSTRAINT gk PRIMARY KEY (author_id, album_id)
+);
+
+CREATE TABLE IF NOT EXISTS Track(
+id INTEGER PRIMARY KEY REFERENCES Album(id),
+name VARCHAR(60) NOT NULL,
+duration INTEGER NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS Collection(
+id SERIAL PRIMARY KEY,
+track_id INTEGER NOT NULL REFERENCES Track(id),
+name VARCHAR(40) NOT NULL,
+released_date date 
+);
+
+
